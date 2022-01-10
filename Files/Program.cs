7e1448fad1db2ea2f155d758f8af571d8de8899f
Blplay13 <95048551+Blplay13@users.noreply.github.com>
2022-01-10 13:37:00 +0200

    Update Program.cs

--- Files/Program.cs
+++ Files/Program.cs
@@ -10,6 +10,23 @@ namespace Files
     {
         static void Main(string[] args)
         {
+             List<string> myWishList = new List<string>();
+
+        Console.WriteLine(myWishList.Count); 
+
+            myWishList.Add("coffee machine");
+            myWishList.Add("a new pair of socks");
+            myWishList.Add("box of chocolates");
+            myWishList.Add("new pillow");
+
+            Console.WriteLine(myWishList.Count);
+            foreach(string wish in myWishList)
+
+            {
+            
+            Console.WriteLine(wish);
+            
+            }
         }
     }
 }
