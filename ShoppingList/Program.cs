e83cdd171403f0ce52fab4798573f1488d604bfd
Blplay13 <95048551+Blplay13@users.noreply.github.com>
2022-01-10 13:38:54 +0200

    Update Program.cs

--- ShoppingList/Program.cs
+++ ShoppingList/Program.cs
@@ -10,6 +10,59 @@ namespace ShoppingList
     {
         static void Main(string[] args)
         {
+            string fileLocation = @$"C:\Users\Tatjana\Samples\shopping";
+            string fileName = @"\\shoppingList.txt";
+
+            string[] arrayFromFile = File.ReadAllLines($"{fileLocation}{fileName}");
+            List<string> shoppingList = arrayFromFile.ToList<string>();
+
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Would you like to add an item to your list? Y/N");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (userInput == 'y')
+                {
+                    Console.WriteLine("Enter an item:");
+                    string newItem = Console.ReadLine();
+                    shoppingList.Add(newItem);
+                }
+                else
+                {
+                    Console.WriteLine("Take care!");
+                    loopActive = false;
+                }
+            }
+
+            Console.Clear();
+
+            foreach (string item in shoppingList)
+            {
+                Console.WriteLine($"You have in your list: {item}");
+            }
+
+            File.WriteAllLines($"{fileLocation}{fileName}", shoppingList);
+
+
+
+        }
+
+        /*public static string ItemsToShop()
+        {
+            List<string> shoppingList = new List<string>();
+            shoppingList.Add("new jeans");
+            shoppingList.Add("a red T-shirt");
+            shoppingList.Add("winter boots for hiking");
+            shoppingList.Add("a cosy sweater");
+            shoppingList.Add("a pair of socks for my partner");
+            shoppingList.Add("automatic feeder for my cat");
+            Console.WriteLine(shoppingList.Count + " items in list:");
+            foreach (string item in shoppingList)
+            {
+                Console.WriteLine(item);
+            }*/
         }
     }
 }
