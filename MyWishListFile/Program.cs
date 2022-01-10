e7fa2f388b3cfda70a204af8df5a1fc39aa77d6a
Blplay13 <95048551+Blplay13@users.noreply.github.com>
2022-01-10 13:38:11 +0200

    Update Program.cs

--- MyWishListFile/Program.cs
+++ MyWishListFile/Program.cs
@@ -10,6 +10,40 @@ namespace MyWishListFile
     {
         static void Main(string[] args)
         {
+            string fileLocation = @"C:\Users\Tatjana\Samples\wishlist";
+            string fileName = @"\\mywishlist.txt";
+
+            string[] arrayFromFile = File.ReadAllLines($"{fileLocation}{fileName}");
+            List<string> myWishList = arrayFromFile.ToList<string>();
+
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Would you like to add a wish? Y/N");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (userInput == 'y')
+                {
+                    Console.WriteLine("Enter your wish:");
+                    string userWish = Console.ReadLine();
+                    myWishList.Add(userWish);
+                }
+                else
+                {
+                    Console.WriteLine("Take care!");
+                    loopActive = false;
+                }
+            }
+
+            Console.Clear();
+
+            foreach (string wish in myWishList)
+            {
+                Console.WriteLine($"You wish: {wish}");
+            }
+
+            File.WriteAllLines($"{fileLocation}{fileName}", myWishList);
         }
     }
 }
